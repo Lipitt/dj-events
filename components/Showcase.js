df223f60f1ab321d439124ab67136751ede84cd6
Lipitt <victor.david.lip@gmail.com>
2021-06-22 20:29:19 -0300

    added showcase component

---
+++ components/Showcase.js
@@ -0,0 +1,9 @@
+import styles from "@/styles/Showcase.module.css";
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Bienvenidos a la fiesta</h1>
+      <h2>Encontra las mejores fiestas de DJs</h2>
+    </div>
+  );
+}
