4d4af89265987d2df61fab1564a7e5bcc9cccdd5
Lipitt <victor.david.lip@gmail.com>
2021-06-22 19:48:36 -0300

    created footer

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; Eventos DJ 2021</p>
+      <p>
+        <Link href="/about">Acerca de este proyecto</Link>
+      </p>
+    </footer>
+  );
+}
