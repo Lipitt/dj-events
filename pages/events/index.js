73b5f2fd6798139da856ae1f4d3ad8de97c4e789
Lipitt <victor.david.lip@gmail.com>
2021-06-23 07:04:15 -0300

    created EventItem component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,30 @@
 import Layout from "@/components/layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>Mis Eventos</h1>
+      {console.log("front: " + events)}
+      <h1>Eventos</h1>
+      {events.length === 0 && <h3>No hay eventos para mostrar</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import Link from "next/link";
 import Layout from "@/components/layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
-export default function HomePage() {
+
+export default function HomePage({ events }) {
   return (
     <Layout>
+      {console.log("front: " + events)}
       <h1>Proximos Eventos</h1>
+      {events.length === 0 && <h3>No hay eventos para mostrar</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">Ver todos los eventos</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -15,6 +29,7 @@ export default function HomePage() {
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
+  console.log(events);
 
   return {
     props: { events },
