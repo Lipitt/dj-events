14dda322b31acde9a9ba47d63cf4b545e17aeadc
Lipitt <victor.david.lip@gmail.com>
2021-06-23 06:22:56 -0300

    added getStaticProps and config file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Layout from "@/components/layout";
+import { API_URL } from "@/config/index";
 export default function HomePage() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Proximos Eventos</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }
