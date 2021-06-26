2417a9b9cd81bcebf3c70bcc7440751fe4a5f9df
Lipitt <victor.david.lip@gmail.com>
2021-06-25 22:29:35 -0300

    added single page events

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,47 @@
 import { API_URL } from "@/config/index";
 import Layout from "@/components/layout";
+import styles from "@/styles/Event.module.css";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function EventPage({ evt }) {
+  const deleteEvent = (e) => {
+    console.log("delete");
+  };
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Editar Evento
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Borrar Evento
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Artistas:</h3>
+        <p>{evt.performers}</p>
+        <h3>Descripcion:</h3>
+        <p>{evt.description}</p>
+        <h3>Lugar: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Volver</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
